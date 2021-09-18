41ccabaf3eeb06b5cf395a93ab74d3d79edfd2a8
tambarie <aclintonadebayo@gmail.com>
2021-09-18 17:31:27 +0100

    The Not Found Page

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const NotFound = () => 
+        <Fragment>
+            <h4>
+                Please, the page you're looking for does not exist
+            </h4>
+        </Fragment>
+   
+
+export default NotFound
