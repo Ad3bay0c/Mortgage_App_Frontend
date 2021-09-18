a85a56f5097104a1cff25d37510a72b07fd3e6ee
tambarie <aclintonadebayo@gmail.com>
2021-09-18 17:34:16 +0100

    The root Js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import Calculator from './components/pages/Calculator';
+import BankState from './context/bank/bankState';
+import Alert from './components/layout/Alert';
+// import ContactState from './context/contact/ContactState';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BankState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className="container">
+            <Alert />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/calculator' component={Calculator} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </BankState>
   );
 }
 
