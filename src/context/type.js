e06c3b267431358c91d7fe16b0b728a494ecbbe8
tambarie <aclintonadebayo@gmail.com>
2021-09-18 17:33:32 +0100

    All the types for the Bank App

---
+++ src/context/type.js
@@ -0,0 +1,16 @@
+export const ADD_CONTACT = 'ADD_CONTACT';
+export const DELETE_CONTACT = 'DELETE_CONTACT';
+export const UPDATE_CONTACT = 'UPDATE_CONTACT';
+export const FILTER_CONTACTS = 'FILTER_CONTACTs';
+export const CLEAR_FILTER = 'CLEAR_FILTER';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
+
+export const CREATE_CONTACT = 'CREATE_CONTACT'
+export const SET_ALERT = 'SET_ALERT'
+export const GET_BANK = 'GET_BANK'
+export const ERASE_BANK = 'ERASE_BANK'
+export const GET_BANKS = 'GET_BANKS'
+export const UPDATE_BANK = 'UPDATE_BANK'
+export const DELETE_BANK = 'DELETE_BANK'
+export const SET_CURRENT = 'SET_CURRENT';
+export const CLEAR_CURRENT = 'CLEAR_CURRENT';
